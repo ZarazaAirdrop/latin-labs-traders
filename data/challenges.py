e2d1345084cb87c279803d26a9fc58011ad9eadb
Zaraza <zaraza@local>
2026-07-08 20:26:53 -0300

    feat: agregar 24-hour Challenge con datos oficiales DNA Funded (10%/4%/6%)

--- data/challenges.py
+++ data/challenges.py
@@ -35,6 +35,14 @@ CHALLENGES = {
         "50000": {"phase1_target": 0, "phase2_target": 0, "daily_loss": 0, "max_loss": 4},
         "100000": {"phase1_target": 0, "phase2_target": 0, "daily_loss": 0, "max_loss": 4},
         "200000": {"phase1_target": 0, "phase2_target": 0, "daily_loss": 0, "max_loss": 4}
+    },
+    "24 Hour": {
+        "5000": {"phase1_target": 10, "phase2_target": 0, "daily_loss": 4, "max_loss": 6},
+        "10000": {"phase1_target": 10, "phase2_target": 0, "daily_loss": 4, "max_loss": 6},
+        "25000": {"phase1_target": 10, "phase2_target": 0, "daily_loss": 4, "max_loss": 6},
+        "50000": {"phase1_target": 10, "phase2_target": 0, "daily_loss": 4, "max_loss": 6},
+        "100000": {"phase1_target": 10, "phase2_target": 0, "daily_loss": 4, "max_loss": 6},
+        "200000": {"phase1_target": 10, "phase2_target": 0, "daily_loss": 4, "max_loss": 6}
     }
 }
 
